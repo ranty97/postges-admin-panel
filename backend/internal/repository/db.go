9418d1a4c8df9879db0754a7361a2418c20ebe25
Ivan Nikalayeu <googacc9091@gmail.com>
2025-04-07 14:03:07 +0300

    feat: back repo

---
+++ backend/internal/repository/db.go
@@ -0,0 +1,71 @@
+package repository
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type DB struct {
+	db *sqlx.DB
+}
+
+func NewDB(db *sqlx.DB) *DB {
+	return &DB{db: db}
+}
+
+func (d *DB) Tables() ([]string, error) {
+
+	query := `
+		SELECT table_name 
+		FROM information_schema.tables 
+		WHERE table_schema='public'
+	`
+
+	var tables []string
+	err := d.db.Select(&tables, query)
+	if err != nil {
+		return nil, fmt.Errorf("postgres: %w", err)
+	}
+	return tables, nil
+}
+
+func (d *DB) ExecuteQuery(query string) (string, error) {
+	if len(query) >= 6 && strings.ToUpper(query[:6]) == "SELECT" {
+		rows, err := d.db.Queryx(query)
+		if err != nil {
+			return "", fmt.Errorf("postgres: %w", err)
+		}
+		defer rows.Close()
+
+		var results []map[string]interface{}
+		for rows.Next() {
+			row := make(map[string]interface{})
+			err := rows.MapScan(row)
+			if err != nil {
+				return "", fmt.Errorf("postgres: %w", err)
+			}
+			results = append(results, row)
+		}
+
+		jsonData, err := json.Marshal(results)
+		if err != nil {
+			return "", fmt.Errorf("json marshal: %w", err)
+		}
+
+		return string(jsonData), nil
+	}
+
+	result, err := d.db.Exec(query)
+	if err != nil {
+		return "", fmt.Errorf("postgres: %w", err)
+	}
+	rows, err := result.RowsAffected()
+	if err != nil {
+		return "", fmt.Errorf("postgres: %w", err)
+	}
+
+	return fmt.Sprintf("rows affected: %d", rows), nil
+}
--- backend/internal/service/db.go
+++ backend/internal/service/db.go
@@ -2,36 +2,33 @@ package service
 
 import (
 	"fmt"
-
-	"github.com/jmoiron/sqlx"
-	_ "github.com/lib/pq"
 )
 
-type DBService struct {
-	db *sqlx.DB
+type Repository interface {
+	Tables() ([]string, error)
+	ExecuteQuery(query string) (string, error)
 }
 
-func NewDBService(db *sqlx.DB) *DBService {
-	return &DBService{db: db}
+type Service struct {
+	repo Repository
 }
 
-func (s *DBService) Tables() ([]string, error) {
-	var tables []string
-	err := s.db.Select(&tables, "SELECT table_name FROM information_schema.tables WHERE table_schema='public'")
+func NewDBService(repo Repository) *Service {
+	return &Service{repo: repo}
+}
+
+func (s *Service) Tables() ([]string, error) {
+	tables, err := s.repo.Tables()
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("repo: %w", err)
 	}
 	return tables, nil
 }
 
-func (s *DBService) ExecuteQuery(query string) (string, error) {
-	result, err := s.db.Exec(query)
+func (s *Service) ExecuteQuery(query string) (string, error) {
+	result, err := s.repo.ExecuteQuery(query)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("repo: %w", err)
 	}
-	rows, err := result.RowsAffected()
-	if err != nil {
-		return "", err
-	}
-	return fmt.Sprintf("Query executed successfully. %d rows affected.", rows), nil
+	return result, nil
 }
